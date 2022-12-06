71b20a8cfdd30d456e017ded26f8c82c80076943
akhilkumar106 <119839773+akhilkumar106@users.noreply.github.com>
2022-12-06 10:17:56 +0530

    #include<stdio.h> int main() {     char t;     scanf("%c",&t);     if(t=='a' || t=='e' || t=='i' || t=='o' || t=='u' || t=='A' || t=='E' || t== 'I' || t== 'O' || t=='U')     printf("VOWEL");     else     printf("CONSONANT");
    }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char t;
+    scanf("%c",&t);
+    if(t=='a' || t=='e' || t=='i' || t=='o' || t=='u' || t=='A' || t=='E' || t== 'I' || t== 'O' || t=='U')
+    printf("VOWEL");
+    else
+    printf("CONSONANT");
+    
+}
