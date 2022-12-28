bba415aa30593be0e4091ed3aa794c678d5a8b19
akhilkumar106 <119839773+akhilkumar106@users.noreply.github.com>
2022-12-28 10:47:56 +0530

    #include<stdio.h> int main() {     int n,k,m,min,max;     scanf("%d%d%d",&n,&k,&m);     min=n/(k*m);     max=min+1;     if(n%(k*m)!=0)     {         printf("%d",max);     }     else     {         printf("%d",min);     } }

---
+++ Bags_and_Candies.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,m,min,max;
+    scanf("%d%d%d",&n,&k,&m);
+    min=n/(k*m);
+    max=min+1;
+    if(n%(k*m)!=0)
+    {
+        printf("%d",max);
+    }
+    else
+    {
+        printf("%d",min);
+    }
+}
