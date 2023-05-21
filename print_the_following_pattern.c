a2ab6c7f4609c2eb06b3ee9dd15cb57a3afb4939
akhilkumar106 <119839773+akhilkumar106@users.noreply.github.com>
2023-05-21 10:04:45 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=0;i<n;i++)     {         for(int j=0;j<n;j++)         {             if((i==j)||(j==0)||(j==n-1))             {                 printf("* ");             }             else             {                 printf("  ");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,18 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int N;
-    scanf("%d",&N);
-    while(N>0)
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
     {
-        for(int i=1;i<=N;i++)
+        for(int j=0;j<n;j++)
         {
-            printf("%d",i);
-            
+            if((i==j)||(j==0)||(j==n-1))
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("  ");
+            }
         }
         printf("
 ");
-        N--;
     }
-    return 0;
 }
